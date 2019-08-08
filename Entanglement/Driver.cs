159fe4cdf51237cd91cc593a5dce6fa7042e9cf8
Chuck <chuck-bates@pluralsight.com>
2019-08-08 11:41:28 -0600

    Initial Commit

---
+++ Entanglement/Driver.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Quantum.Simulation.Simulators;
+using Quantum.Entanglement;
+
+namespace Entanglement
+{
+    class Driver
+    {
+        static void Main(string[] args)
+        {
+            using (var simulator = new QuantumSimulator())
+            {
+                var results = Measurement.Run(simulator).Result;
+                Console.WriteLine($"Measured result states: {results}.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Entanglement/Operations.qs
@@ -0,0 +1,21 @@
+﻿namespace Quantum.Entanglement
+{
+    open Microsoft.Quantum.Intrinsic;
+    open Microsoft.Quantum.Canon;
+    
+    operation Measurement () : (Result)
+    {
+        mutable result = (Zero);
+
+        using(qubits = Qubit[1]) {
+
+            let qubit = qubits[0];
+				
+            set result = (M(qubit));
+
+            ResetAll(qubits);
+        }
+
+        return result;
+    }
+}
